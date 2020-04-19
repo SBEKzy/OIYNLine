05b3326d8eba76bddf0bfbe5e784845ced41848f
FlashFox12 <32808565+FlashFox12@users.noreply.github.com>
2020-04-19 14:55:11 +0600

    Revert "Websocket"

--- model/User.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type User struct {
-	ID       int    `gorm:"primary_key;auto_increment" json:"id"`
-	Email    string `gorm:"size:255;not null;unique" json:"email"`
-	Password string `gorm:"size:100;not null;" json:"password"`
-	Username string `gorm:"size:100;not null;unique" json:"username"`
-}
