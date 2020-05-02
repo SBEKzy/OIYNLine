a90ee94ba7724651b64cef095a589ee3893cd51a
Bekzat <s.bekzattt@gmail.com>
2020-05-02 16:01:23 +0600

    fix

--- model/Game.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Game struct {
-	ID          int    `gorm:"primary_key;auto_increment" json:"id"`
-	Name        string `gorm:"size:255;" json:"name"`
-	Description string `gorm:"size:255;" json:"description"`
-	Rules       string `gorm:"size:255;" json:"rules"`
-}
--- model/MyGames.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type MyGames struct {
-	ID      int `gorm:"primary_key;auto_increment" json:"id"`
-	GamerID int `gorm:"size:255;" json:"gameid"`
-	GameID  int `gorm:"size:255;" json:"gemerid"`
-}
